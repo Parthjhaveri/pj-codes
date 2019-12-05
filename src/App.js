32246f607f2694009c0b717d96cb39ab6fe2d0cc
Parthjhaveri <parthjhaveri209@gmail.com>
2019-12-04 20:11:09 -0500

    got data from firebase for skillset

--- src/App.js
+++ src/App.js
@@ -12,12 +12,32 @@ class App extends React.Component {
 		super(props);
 
 		this.state = {
-
+			skills_arr: []
 		}
 	}
 
 	componentDidMount() {	
-		const rootRef = firebase.database().ref().child('react');
+
+		// GET LIST HOME SKILLSET FROM FIREBASE
+		const db = firebase.firestore();
+		var skillset_ref = db.collection('skillset').doc('B5V0RPKajYlvhbSagIEY');
+
+		skillset_ref.get()
+
+		.then(function(doc) {
+		    if (doc.exists) {
+		        console.log("Document data:", doc.data());
+		    } else {
+		        // doc.data() will be undefined in this case
+		        console.log("No such document!");
+		    }
+		})
+
+		.catch(function(err) {
+		    console.log(err);
+		});
+
+		
 	}
 
 	render() {
